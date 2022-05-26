d3537835aa7f2d8765c37e5d7ac7ead7d945e3a1
MarkLewisII <77941914+MarkLewisII@users.noreply.github.com>
2022-05-26 11:37:38 -0400

    Got Score Totalling working

--- greedy.js
+++ greedy.js
@@ -1,15 +1,30 @@
-// function ScoreTotals() {
-//     let
-// }
+var tally = 0;
+var game_score = 0;
+
+function Score_Totals() {
+    game_score +=  tally;
+    document.getElementById("Player_Total").innerHTML = `Total: ${game_score}`;
+}
+
 function Round_Score(score) {
-    var tally = 0;
-    
+    // var tally = 0;    
+    if (score === 0) {
+        tally = 0;
+    } else {
         tally += score;
+    }
+
     document.getElementById("Player_Round_Score").innerHTML = `Round Score: ${tally}`;
+
+    Score_Totals();
 }
 
 function Dice_Roll() {
     let current_roll = Math.floor((Math.random() * 6) + 1);
 
-    Round_Score(current_roll);
+    if (current_roll === 1) {
+        Round_Score(0);
+    } else {
+        Round_Score(current_roll);
+    }
 }
